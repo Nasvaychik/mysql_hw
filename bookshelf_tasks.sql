360eb775d9117243a6321e33c1f2615ca851f863
Nasvaychik <118043121+Nasvaychik@users.noreply.github.com>
2023-05-04 00:44:07 +0300

    Add files via upload

---
+++ bookshelf_tasks.sql
@@ -0,0 +1,78 @@
+-- 1. Найти автора с самым большим числом книг и вывести его имя
+SELECT authors.name FROM authors
+JOIN authors_books ON authors_books.authors_id = authors.id
+GROUP BY authors.name
+ORDER BY COUNT(authors_books.authors_id) DESC
+LIMIT 1;
+
+-- 2. Вывести пять самых старых книг у которых указан год издания
+SELECT * FROM books
+WHERE books.year IS NOT NULL
+ORDER BY books.year ASC
+LIMIT 5;
+
+-- 3. Вывести общее количество книг на полке в кабинете
+SELECT COUNT(books.id) as count FROM books
+JOIN shelves ON books.shelves_id = shelves.id
+WHERE shelves.title = 'Полка в кабинете'
+GROUP BY books.shelves_id;
+
+-- 4. Вывести названия, имена авторов и годы издания книг, которые находятся на полке в спальне
+SELECT books.title, authors.name, books.year FROM books
+JOIN shelves ON shelves.id = books.shelves_id
+JOIN authors_books ON authors_books.books_id = books.id
+JOIN authors ON authors_books.authors_id = authors.id
+WHERE shelves.title = 'Полка в спальне';
+
+-- 5. Вывести названия и годы издания книг, написанных автором 'Лев Толстой'
+SELECT books.title, books.year FROM books
+JOIN shelves ON shelves.id = books.shelves_id
+JOIN authors_books ON authors_books.books_id = books.id
+JOIN authors ON authors_books.authors_id = authors.id
+WHERE authors.name = 'Лев Толстой';
+
+-- 6. Вывести название книг, которые написали авторы, чьи имена начинаются на букву "А"
+SELECT books.title FROM books
+JOIN shelves ON shelves.id = books.shelves_id
+JOIN authors_books ON authors_books.books_id = books.id
+JOIN authors ON authors_books.authors_id = authors.id
+WHERE authors.name LIKE 'А%';
+
+-- 7. Вывести название книг и имена авторов для книг, которые находятся на полках, названия которых включают слова «верхняя» или «нижняя»
+SELECT books.title, authors.name FROM books
+JOIN shelves ON shelves.id = books.shelves_id
+JOIN authors_books ON authors_books.books_id = books.id
+JOIN authors ON authors_books.authors_id = authors.id
+WHERE shelves.title LIKE '%нижняя%' OR shelves.title LIKE '%верхняя%';
+
+-- 8. Книгу «Божественная комедия» автора «Данте Алигьери» одолжили почитать другу Ивану Иванову, необходимо написать один или несколько запросов которые отразят это событие в БД
+UPDATE books
+JOIN authors_books ON authors_books.books_id = books.id
+JOIN authors ON authors_books.authors_id = authors.id
+SET books.friends_id = (SELECT friends.id FROM friends WHERE friends.name = 'Иванов Иван')
+WHERE authors.name = 'Данте Алигьери' AND books.title = 'Божественная комедия'
+AND books.id > 0;
+
+-- 9. Добавить в базу книгу «Краткие ответы на большие вопросы», год издания 2020, автор «Стивен Хокинг», положить ее на полку в кабинете
+INSERT INTO authors 
+(name)
+VALUES 
+(
+	"Стивен Хокинг"
+);
+
+INSERT INTO books 
+(title, year, shelves_id)
+VALUES (
+	"Краткие ответы на большие вопросы", 
+    2020, 
+    (SELECT shelves.id FROM shelves WHERE shelves.title = "Полка в кабинете" LIMIT 1)
+);
+
+INSERT INTO authors_books
+(books_id, authors_id)
+VALUES 
+(
+	(SELECT books.id FROM books WHERE books.title = "Краткие ответы на большие вопросы" AND books.year = 2020 LIMIT 1),
+	(SELECT authors.id FROM authors WHERE authors.name = "Стивен Хокинг" LIMIT 1)
+);
